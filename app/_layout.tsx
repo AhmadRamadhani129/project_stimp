38461a23959e6e24078f7275280e5610b98d9775
AhmadRamadhani129 <ahmad.ramadhani.aribowo@gmail.com>
2024-10-10 23:25:13 +0700

    Login, Menu, Game
    1. Sudah bisa Login dan Logout
    2. Sudah ada menu sederhana
    3. Sudah bisa pindah ke menu game

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,33 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { AuthProvider, useAuth } from "./authContext";
+import { useEffect } from "react";
 
-export default function RootLayout() {
+function RootLayout() {
+  const { isLoggedIn } = useAuth(); // Get logged-in status
+  const router = useRouter();
+  useEffect(() => {
+    // Redirect based on login status
+    if (!isLoggedIn) {
+      // If not logged in, redirect to login screen
+      router.replace("/login");
+    } else {
+      // If logged in, redirect to index
+      router.replace("/");
+    }
+  }, [isLoggedIn]);
+
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="game" options={{ title: "Play Game" }} />
     </Stack>
+  );
+}
+
+export default function Layout() {
+  return (
+    <AuthProvider>
+      <RootLayout />
+    </AuthProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,69 @@
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, Text, View, StyleSheet, ScrollView } from "react-native";
+import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useAuth } from "./authContext";
 
 export default function Index() {
+
+  const router = useRouter();
+
+  const [username, setUsername] = useState<string>("");
+  const { logout } = useAuth();
+
+  const cekLogin = async () => {
+    try {
+      const value = await AsyncStorage.getItem("username");
+      if (value !== null) {
+        setUsername(value);
+      } else {
+        setUsername("");
+        logout();
+      }
+    } catch (e) {
+      console.error("Error reading username from AsyncStorage", e);
+      setUsername("");
+      logout();
+    }
+  };
+
+  const doLogout = async () => {
+    try {
+      await AsyncStorage.removeItem("username");
+      alert("logged out");
+      logout();
+    } catch (e) {}
+  };
+
+  useEffect(() => {
+    cekLogin();
+  }, [username]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <Text style={{ color: "red", fontSize: 30 }}>Memory Pattern</Text>
+      <Button title="Go to Game" onPress={() => router.push("/game")} />
+      <Button title="Logout" onPress={() => doLogout()} />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  container: {
+    marginBottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonRow: {
+    flexDirection: "row",
+    marginTop: 20,
+    justifyContent: "space-between",
+    width: "80%",
+  },
+});
